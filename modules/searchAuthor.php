7b407ce5c56f3eb15605fae2fdf14e3da90e8ea2
Alexander Babin <41280415+brightgreenban@users.noreply.github.com>
2018-09-15 14:39:28 +0300

    Update searchAuthor.php

--- modules/searchAuthor.php
+++ modules/searchAuthor.php
@@ -2,7 +2,7 @@
 
 	$author = $_POST['author'];
 	
-	$sql = mysql_query("SELECT * FROM `book_list` WHERE `author` LIKE '".$author."%'", $link);
+	$sql = mysql_query("SELECT * FROM `book_list` WHERE `author` LIKE '%".$author."%'", $link);
 	
 		while ($row = mysql_fetch_array($sql, MYSQL_NUM)): include("modules/get_data.php");
 endwhile;
